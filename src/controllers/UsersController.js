3d3e7465222cfe66a7cab8cac9c16ed8cde886ba
shuharib0t <shuharib0t@gmail.com>
2023-06-09 00:42:41 -0300

    add update in UsersController

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,29 +1,67 @@
-const AppError = require("../utils/AppError");
+const { hash } = require("bcryptjs");
+
 const knex = require("../database/knex");
-const sqliteConnection = require("../database/sqlite");
+const AppError = require("../utils/AppError");
 
 class UsersController {
   async create(req, res) {
     const { name, email, password } = req.body;
 
-    const check = await knex
+    const checkEmail = await knex
       .select("*")
       .from("users")
-      .where("email", "LIKE", `%${email}%`);
+      .whereLike("email", `%${email}%`);
 
-    console.log(check);
-
-    if (check.length) {
+    if (checkEmail.length) {
       throw new AppError("E-mail already exists");
     }
+
+    const hashedPassword = await hash(password, 10);
 
     await knex("users").insert({
       name,
       email,
-      password,
+      password: hashedPassword,
     });
 
-    res.status(201).json({ name, email, password });
+    res.status(201).json();
+  }
+
+  async update(req, res) {
+    const { name, email } = req.body;
+    const { id } = req.params;
+
+    const user = await knex
+      .select("*")
+      .from("users")
+      .where("id", "LIKE", `%${id}%`);
+
+    if (!user.length) {
+      throw new AppError("User not found");
+    }
+
+    const userWithEmail = await knex
+      .select("*")
+      .from("users")
+      .where("email", "LIKE", `%${email}%`);
+
+    const isEmailFromDiferentUser = userWithEmail.find(
+      (userEmail) => userEmail.email !== user[0].email
+    );
+
+    if (isEmailFromDiferentUser) {
+      throw new AppError("E-mail already exists");
+    }
+
+    user.name = name;
+    user.email = email;
+
+    await knex("users").where("id", id).update({
+      name: user.name,
+      email: user.email,
+    });
+
+    res.status(201).json();
   }
 }
 
--- src/database/knex/migrations/20230607170954_createUsers.js
+++ src/database/knex/migrations/20230607170954_createUsers.js
@@ -1,12 +1,17 @@
 exports.up = (knex) =>
-  knex.schema.createTable("users", (table) => {
-    table.increments("id").primary();
-    table.text("name");
-    table.text("email");
-    table.text("password");
+  knex.schema.hasTable("users").then((exists) => {
+    if (!exists) {
+      return knex.schema.createTable("users", (table) => {
+        table.increments("id").primary();
+        table.string("name");
+        table.string("email");
+        table.string("password");
+        table.string("avatar");
 
-    table.timestamp("created_at").default(knex.fn.now());
-    table.timestamp("updated_at").default(knex.fn.now());
+        table.timestamp("created_at").default(knex.fn.now());
+        table.timestamp("updated_at").default(knex.fn.now());
+      });
+    }
   });
 
 exports.down = (knex) => knex.schema.dropTable("users");
