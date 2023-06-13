6601a9f8c15bbc09bdbc27a688b639635b1b5ea2
shuharib0t <shuharib0t@gmail.com>
2023-06-13 15:08:53 -0300

    add TagsController

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -50,7 +50,16 @@ class NotesController{
       const filterTags = tags.split(',').map(tag => tag.trim());
       
       notes = await knex("tags")
-        .whereIn("title", filterTags)
+        .select([
+          "notes.id",
+          "notes.title",
+          "notes.user_id",
+        ])
+        .where("notes.user_id", user_id)
+        .whereLike("notes.title", `%${title}%`)
+        .whereIn("tags.title", filterTags)
+        .innerJoin("notes", "notes.id", "tags.note_id")
+        .orderBy("notes.title");
 
     } else {
       notes = await knex("notes")
@@ -59,7 +68,17 @@ class NotesController{
         .orderBy("title");
     }
 
-    return res.status(201).json(notes);
+    const userTags = await knex("tags").where({ user_id });
+    const noteWithTags = notes.map(note => {
+      const noteTags = userTags.filter(tag => tag.note_id === note.id);
+
+      return {
+        ...note,
+        tags: noteTags,
+      }
+    })
+
+    return res.status(201).json(noteWithTags);
   }
 }
 
